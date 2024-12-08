e7acf9dfca0eac3caf8982dedf737aca8e06748c
Hizbucodes <106305260+Hizbucodes@users.noreply.github.com>
2024-12-08 23:45:14 +0530

    add addHabit action

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { Container, Typography } from "@mui/material";
+import AddHabitForm from "./components/AddHabitForm";
+import HabitList from "./components/HabitList";
 
 function App() {
   return (
     <Provider store={store}>
-      <div>Redux</div>
+      <Container maxWidth="md">
+        <Typography component="h1" variant="h2" align="center">
+          Habit Tracker
+        </Typography>
+        <AddHabitForm />
+        <HabitList />
+      </Container>
     </Provider>
   );
 }
---
+++ src/components/HabitList.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+import { Box, Grid, Paper, Typography } from "@mui/material";
+
+const HabitList: React.FC = () => {
+  const { habits } = useSelector((state: RootState) => state.habits);
+
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 4 }}>
+      {habits.map((habit) => {
+        return (
+          <Paper key={habit.id} elevation={2} sx={{ p: 2 }}>
+            <Grid container alignItems={"center"}>
+              <Grid>
+                <Typography variant="h6">{habit.name}</Typography>
+                <Typography variant="h6">{habit.frequency}</Typography>
+              </Grid>
+            </Grid>
+          </Paper>
+        );
+      })}
+    </Box>
+  );
+};
+
+export default HabitList;
